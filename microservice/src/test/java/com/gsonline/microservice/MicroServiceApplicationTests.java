60f9c6dbb11098cfc276df3f5e5d80a982d9e088
gustavsr1994 <gustavr1994@gmail.com>
2020-09-06 13:51:41 +0700

    first commit

---
+++ microservice/src/test/java/com/gsonline/microservice/MicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsonline.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
